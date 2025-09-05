a42d78a4a16aecce1dbeef9c125e59833d09d6c8
Camilo Rincon <camilo.rincon.pineda@gmail.com>
2025-09-04 21:59:48 -0500

    Feat: Temporarily disable minification in vite.config.ts for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    minify: false, // Temporarily disable minification for debugging
+  },
 }));
