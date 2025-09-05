46253e50c7f2773fef42019aa3abdd361863d76e
Camilo Rincon <camilo.rincon.pineda@gmail.com>
2025-09-04 21:54:13 -0500

    Feat: Reintroduce App component and index.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-console.log("TOP OF main.tsx is executing!"); // New log
-
 import { createRoot } from 'react-dom/client'
-// import App from './App.tsx' // Comment out
-// import './index.css' // Comment out
+import App from './App.tsx' // Reintroduce
+import './index.css' // Reintroduce
 
-console.log("main.tsx is executing!"); // Existing log
+// console.log("TOP OF main.tsx is executing!"); // Remove this log
+// console.log("main.tsx is executing!"); // Remove this log
 
 createRoot(document.getElementById("root")!).render(
-  // <App /> // Comment out
-  <div>Hello from main.tsx!</div> // Simple div instead of App
+  <App /> // Reintroduce
 );
